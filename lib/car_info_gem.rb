2ca8890965912425b712ebc2d1840e72a250e9ed
Edwin Ramirez <eramirez@growthaccelerationpartners.com>
2018-05-08 11:45:14 -0500

    First commit

---
+++ lib/car_info_gem.rb
@@ -0,0 +1,9 @@
+require "car_info_gem/version"
+
+module CarInfoGem
+  class WhoIs
+    def self.awesome?
+      puts "YOU ARE AWESOME!!"
+    end
+  end
+end
---
+++ lib/car_info_gem/version.rb
@@ -0,0 +1,3 @@
+module CarInfoGem
+  VERSION = "0.1.0"
+end
